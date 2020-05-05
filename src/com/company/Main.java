95347815cc02f5bfb6ae0cf125fc36a1e8f5ecd9
eddyvu <dicoor2604@gmail.com>
2020-05-05 17:54:44 +0700

    2nd commit: using (long)Math.sqrt(n) to optimize

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -51,7 +51,7 @@ public class Main {
         if(n==1){
             return false;
         }
-        for(int i=2;i<n/2;i++){
+        for(int i=2;i<(long)Math.sqrt(n);i++){
             if(n%i ==0) {
                 return false;
             }
